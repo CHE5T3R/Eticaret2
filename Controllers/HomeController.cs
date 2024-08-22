2c992509b749601a5702568d7c06c021c2745d7f
u210709033 <dogukansandikci.53.53@gmail.com>
2024-08-22 16:36:06 +0300

    Proje dosyası ekle.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Eticaret.Models.Context;
+using Eticaret.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eticaret.Controllers
+{
+    public class HomeController : Controller
+    {
+        DataContext context = new DataContext();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var items = context.Products
+                .Where(i => i.IsHome && i.IsApproved)
+                .Select(i => new ProductModel()
+                {
+                    Id = i.Id,
+                    Name = i.Name.Length > 50 ? i.Name.Substring(0, 47) + "..." : i.Name,
+                    Description = i.Description.Length > 50 ? i.Description.Substring(0, 47) + "..." : i.Description,
+                    Price = i.Price,
+                    Stock = i.Stock,
+                    Image = i.Image ?? "1.jpg",
+                    CategoryId = i.CategoryId
+                })
+                .ToList();
+
+            return View(items);
+        }
+        public ActionResult Details(int id)
+        {
+            return View(context.Products.Where(i => i.Id == id).FirstOrDefault());
+        }
+        public ActionResult List(int?id)
+        {
+            var items = context.Products
+                .Where(i => i.IsApproved)
+                .Select(i => new ProductModel()
+                {
+                    Id = i.Id,
+                    Name = i.Name,
+                    Description = i.Description.Length > 50 ? i.Description.Substring(0, 47) + "..." : i.Description,
+                    Price = i.Price,
+                    Stock = i.Stock,
+                    Image = i.Image ?? "1.jpg",
+                    CategoryId = i.CategoryId
+                }).AsQueryable();
+            if (id != null)
+            { 
+                items = items.Where(i=>i.CategoryId == id);
+            }
+
+            return View(items.ToList());
+        }
+        public PartialViewResult GetCategories()
+        {
+            return PartialView(context.Categories.ToList());
+        }
+    }
+}
