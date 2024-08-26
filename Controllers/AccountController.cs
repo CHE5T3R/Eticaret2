11bc68b68e6d5dd9624d44846fb3650a38ea99e4
u210709033 <dogukansandikci.53.53@gmail.com>
2024-08-26 17:57:41 +0300

    son video

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
